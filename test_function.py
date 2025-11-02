758409ae67fddb86da65e0190f062a67531660c6
Sergi <slabordaghiringhelli@gmail.com>
2025-11-02 22:46:21 +0100

    created a unit step function

--- test_function.py
+++ test_function.py
@@ -1,1 +1,16 @@
 print("hello")
+import numpy as np
+import matplotlib.pyplot as plt
+
+def unit_step_function(t):
+    #creates a function that returns 1 when t>=0 and returns 0 elsewhere
+    return np.where(t>0, 1, 0)
+    
+t = np.linspace(-10,20)
+u = unit_step_function(t)
+
+
+
+plt.plot(t,u)
+plt.show()
+
