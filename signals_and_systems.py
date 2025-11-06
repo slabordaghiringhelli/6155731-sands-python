0e816a203356c4a2cefbee133c540743dfa23c8c
Sergi <slabordaghiringhelli@gmail.com>
2025-11-06 22:48:01 +0100

    I added a few docstrings for the defined functions. Also I time scaled the periodic square wave function and plotted another function z which is the product of the unit step function u and the periodic square wave function

--- signals_and_systems.py
+++ signals_and_systems.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 
 def create_sine_wave(frequency, duration):
+    """This function yields a sine wave for a given frequency and duration"""
     t = np.linspace(0, duration, int(44100 * duration))
     return np.sin(2 * np.pi * frequency * t)
     
@@ -24,27 +25,58 @@ plt.show()
 
 print("hello")
 
+#Unit step function
 
 def unit_step_function(t):
-    #creates a function that returns 1 when t>=0 and returns 0 elsewhere
+    """creates a function that returns 1 when t>=0 and returns 0 elsewhere"""
     return np.where(t>0, 1, 0)
     
 t = np.linspace(-10,20)
-u = unit_step_function(t)
-
-
-plt.plot(t,u)
-plt.show()
+u_values = unit_step_function(t)
 
 
 
+
+
+#Periodic square wave function
 
 def square_wave(t, period=1) :
+    """Creates a periodic square wave function.
+     The time has been defined using an np.linspace() function from 0 to 10 using a 100 points in that aforementioned interval."""
     t_periodic = t % 1  #Here I ensure that the time in the function is periodic
     return np.where(t_periodic<period/2,1,-1)
-t = np.linspace(-5,5,1000)
+t = np.linspace(0,10,100)
 y = square_wave(t, period=1)   
 
 
 plt.plot(t,y)
+plt.show()
+
+
+
+#Time scaling
+
+u_new = unit_step_function(t-2)
+
+plt.plot(u_new)
+plt.show()
+
+
+t_shift = 2
+
+y_new = -2*square_wave(t-t_shift, period=1)
+
+plt.plot(t, y_new)
+plt.ylim(-3,3)
+plt.show()
+
+
+#Multiplication of two different functions
+
+z = unit_step_function(t)*y
+plt.plot(t,z)
+plt.title("z=u(t)·y")
+plt.xlabel("time/t")
+plt.ylabel("z")
+plt.xlim(0,5)
 plt.show()
--- test_function.py
+++ test_function.py
@@ -5,6 +5,29 @@ import matplotlib.pyplot as plt
 
 
 
+def square_wave(t, period=1) :
+    t_periodic = t % 1  #Here I ensure that the time in the function is periodic
+    return np.where(t_periodic<period/2,1,-1)
+t = np.linspace(-5,5,1000)
+y = square_wave(t, period=1)   
+
+y = square_wave(t, period=1)
+assert y[1] == 1
+
+
+
+def unit_step_function(t):
+    #creates a function that returns 1 when t>=0 and returns 0 elsewhere
+    return np.where(t>0, 1, 0)
+    
+t = np.linspace(-10,20)
+u = unit_step_function(t)
+
+assert u[19] == 1
+
+
+
+
 
 
 
